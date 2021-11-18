d0a5b0db1e8aa7ed8f9ba52908dae1e956e62891
Schmidt <12001001973813@FEDIDCARD.GOV>
2021-11-18 11:59:21 -0500

    Update soilsort2021-refresh.r

--- soilsort2021-refresh.r
+++ soilsort2021-refresh.r
@@ -352,5 +352,6 @@ s$Site<-ifelse(s$Site %in% "Not",
                                                            grepl('ist', s$taxsubgrp),'F144BY504ME','F144BY501ME'))))))
                ,s$Site)
 
-write.csv(s,'fy2021-refresh/s.144B.csv', row.names = F)
+s.nolmapunit<- unique(subset(s, select=-c(lmapunitiid,percentmlra,pmlra,mu.total)))
+write.csv(s.nolmapunit,'fy2021-refresh/s.144B.csv', row.names = F)
 #s <- merge(s.lmu, s, by='muiid')
